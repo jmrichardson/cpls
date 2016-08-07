2f45a2ffb72e09d2e04f400d6268bb7b9c1236f9
jmrichardson <jmrichardson@gmail.com>
2016-08-07 00:23:31 -0400

    terst

--- build/build.sh
+++ build/build.sh
@@ -1,23 +1,31 @@
 # Automated build based on architecture
 
-user=`whoami`
-if [ ! -d /home/$user/store ]; then
-  echo "Creating /home/$user/store directory"
-  mkdir /home/$user/store
-  chmod 751 cpls
+dir=`pwd`
+base=`basename $dir`
+if [ base != 'build' ]; then
+  echo "Err: Must be in build directory"
+  exit
 fi
-if [ ! -f /home/$user/store/config.R ]; then
+
+if [ ! -d $HOME/store ]; then
+  echo "Creating $HOME/store directory"
+  mkdir $HOME/store
+  chmod 751 $HOME/store
+fi
+if [ ! -f $HOME/store/config.R ]; then
   echo "Copying default config.R"
-  docker run -u user:user -v /home/$user/store:/home/user/cpls/store cpls cp /home/user/cpls/data/config.R /home/user/cpls/store
+  docker run -u user:user -v $HOME/store:/home/user/cpls/store cpls cp /home/user/cpls/data/config.R /home/user/cpls/store
   echo "Copying default user_name.acc"
-  docker run -u user:user -v /home/$user/store:/home/user/cpls/store cpls cp /home/user/cpls/data/user_name.acc /home/user/cpls/store 
+  docker run -u user:user -v $HOME/store:/home/user/cpls/store cpls cp /home/user/cpls/data/user_name.acc /home/user/cpls/store 
 fi
 
 arch | grep arm > /dev/null
 if [ $? -eq 0 ]; then
-  cd arm
+  file='./build/arm/Dockerfile'
 else
-  cd linux
+  file='./build/linux/Dockerfile'
 fi
 
-docker build -t cpls .  
+cd ..
+docker build -t cpls -f $file .
+
